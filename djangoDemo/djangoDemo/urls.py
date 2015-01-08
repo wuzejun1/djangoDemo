7e286473602c6b75f7f66370a12b0aa8469cb3fe
wuzejun <wuzejun1@gmail.com>
2015-01-09 00:15:09 +0800

    django View demo

--- djangoDemo/djangoDemo/urls.py
+++ djangoDemo/djangoDemo/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^polls/', include('polls.urls')),
     # Examples:
     # url(r'^$', 'djangoDemo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
